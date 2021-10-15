21ae18921bfdd2e436e4cd1e6ff745bd3c402d79
Navneet Lal Gupta <navneetlalg@gmail.com>
2021-10-15 20:40:45 +0530

    add refresh on new pdf

--- server/src/lib/initialize.ts
+++ server/src/lib/initialize.ts
@@ -4,10 +4,17 @@ import Investor from "../models/investor"
 import Folio from "../models/folio"
 import Scheme from "../models/scheme"
 import Transaction from "../models/transactions"
+import Valuation from "../models/valuation"
 
 function initialize(report: any) {
   const { investor_info, folios } = report
   return sequelize.transaction(async transaction => {
+    await Investor.destroy({
+      where: {
+        email: investor_info.email
+      },
+      truncate: true
+    })
     const investor = await Investor.create({
       name: investor_info.name,
       email: investor_info.email,
@@ -33,11 +40,24 @@ function initialize(report: any) {
         type: scheme.type,
         advisor: scheme.advisor,
         rtaCode: scheme.rta_code,
-        rta: scheme.rta,
-        valuation: scheme.valuation
+        rta: scheme.rta
       }))
     })
     await Scheme.bulkCreate(schemeRefactored, { transaction })
+
+    const valuationRefactored = folios.flatMap((folio: any) => {
+      return folio.schemes.map((scheme: any) => ({
+        amfi: scheme.amfi,
+        date: new Date(scheme.valuation.date),
+        amount: scheme.transactions.reduce(function (accumulator: number, item: any) {
+          return accumulator + parseFloat(item.amount);
+        }, 0),
+        value: scheme.valuation.value,
+        nav: scheme.valuation.nav,
+        balance: scheme.transactions.filter((t: any) => !!t.balance).reverse()[0].balance ?? 0
+      }))
+    })
+    Valuation.bulkCreate(valuationRefactored, { transaction })
 
     const transactionRefactored = folios.flatMap((folio: any) => {
       return folio.schemes.flatMap((scheme: any) => {
